ba6b8d1e4250cfe6aa38103bd9e454059011ad40
Daniele <daniele.stirp@gmail.com>
2016-09-07 11:34:43 +0200

    No need storing serialized Objects

--- app/src/main/java/com/epresidential/rebtest/rest/JsonObject.java
+++ app/src/main/java/com/epresidential/rebtest/rest/JsonObject.java
@@ -5,6 +5,5 @@ import java.io.Serializable;
 /**
  * Created by daniele on 01/09/16.
  */
-public abstract class JsonObject implements Serializable {
-    private static final long serialVersionUID = 1L;
+public abstract class JsonObject{
 }
